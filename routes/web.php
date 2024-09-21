4678c6ffbb9a39cf89162d93a51828679fbbb924
jiannsyah <brian8jian@gmail.com>
2024-09-21 14:54:13 +0700

    make a table & model Project & Task

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Homepage', [
-        'title' => 'Test Apk',
-    ]);
-});
-Route::get('/welcome', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
