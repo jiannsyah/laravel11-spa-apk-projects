7fb84750ca21f5163170e80dee2eb991de59391c
jiannsyah <brian8jian@gmail.com>
2024-09-22 09:38:42 +0700

    add filtering input name & select status

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,8 +1,27 @@
 import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constant";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head, Link } from "@inertiajs/react";
+import { Head, Link, router } from "@inertiajs/react";
 
-export default function Index({ projects }) {
+export default function Index({ projects, queryParams = null }) {
+  queryParams = queryParams || {};
+  const searchFieldChange = (name, value) => {
+    if (value) {
+      queryParams[name] = value;
+    } else {
+      delete queryParams[name];
+    }
+
+    router.get(route("project.index"), queryParams);
+  };
+
+  const onKeyPress = (name, e) => {
+    if (e.key !== "Enter") return;
+
+    searchFieldChange(name, e.target.value);
+  };
   return (
     <AuthenticatedLayout
     // header={
@@ -30,6 +49,41 @@ export default function Index({ projects }) {
                     <th className="px-3 py-3">Actions</th>
                   </tr>
                 </thead>
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr className="text-nowrap">
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3">
+                      <TextInput
+                        defaultValue={queryParams.name}
+                        className="w-full"
+                        placeholder="Project Name"
+                        onBlur={(e) =>
+                          searchFieldChange("name", e.target.value)
+                        }
+                        onKeyPress={(e) => onKeyPress("name", e)}
+                      />
+                    </th>
+                    <th className="px-3 py-3">
+                      <SelectInput
+                        defaultValue={queryParams.status}
+                        className="w-full"
+                        onChange={(e) =>
+                          searchFieldChange("status", e.target.value)
+                        }
+                      >
+                        <option value="">Status</option>
+                        <option value="pending">Pending</option>
+                        <option value="in_progress">In Progress</option>
+                        <option value="completed">Completed</option>
+                      </SelectInput>
+                    </th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                  </tr>
+                </thead>
                 <tbody>
                   {projects.data.map((project) => (
                     <tr
@@ -45,7 +99,16 @@ export default function Index({ projects }) {
                         />
                       </td>
                       <td className="px-3 py-2">{project.name}</td>
-                      <td className="px-3 py-2">{project.status}</td>
+                      <td className="px-3 py-2">
+                        <span
+                          className={
+                            "px-2 py-1 rounded text-white " +
+                            PROJECT_STATUS_CLASS_MAP[project.status]
+                          }
+                        >
+                          {PROJECT_STATUS_TEXT_MAP[project.status]}
+                        </span>
+                      </td>
                       <td className="px-3 py-2">{project.created_at}</td>
                       <td className="px-3 py-2">{project.due_date}</td>
                       <td className="px-3 py-2">{project.created_by.name}</td>
